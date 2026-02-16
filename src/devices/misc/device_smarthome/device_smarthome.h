1b6ca8893f55c3d33683ac4e57fb632973fb07b0
Chuck Rohs <rohs@shaw.ca>
2026-02-16 00:48:50 -0700

    HA gui updates

--- src/devices/misc/device_smarthome/device_smarthome.h
+++ src/devices/misc/device_smarthome/device_smarthome.h
@@ -5,4 +5,10 @@ extern uint16_t SMARTHOME_MQTT_BULB2_SET;
 extern uint16_t SMARTHOME_MQTT_BULB1_BRIGHTNESS_SET;
 extern uint16_t SMARTHOME_MQTT_BULB2_BRIGHTNESS_SET;
 
-void register_device_smarthome();
+// WebSocket equivalents
+extern uint16_t SMARTHOME_WS_LIGHT_ON;
+extern uint16_t SMARTHOME_WS_LIGHT_OFF;
+extern uint16_t SMARTHOME_WS_LIGHT_BRIGHTNESS;
+
+void
+register_device_smarthome();
